77749a13e65c9dbc13d68664f8445341145cca79
AdorableHummingbird <48790098+AdorableHummingbird@users.noreply.github.com>
2024-02-20 20:13:25 +0530

    renamed the input variable

--- long_int_to_date.c
+++ long_int_to_date.c
@@ -9,32 +9,32 @@ int period_conv(long,int *, int *);
 
 int main(void)
 {
-    long n;
+    long input;
     int year, date, mon;
     char *mon_data[20]={"January","February","March","April","May","June","July","August","September","October","November","December"};
     char *month;
     printf("Please enter the period in long form: ");
-    scanf("%ld",&n);        /*15081945 */
-    mon=period_conv(n,&year,&date);
+    scanf("%ld",&input);        /*15081945 */
+    mon=period_conv(input,&year,&date);
     if(mon>12)
     {
         printf("Incorrect Period\nRe-enter Period: ");
-        scanf("%ld",&n);
-        mon=period_conv(n,&year,&date);
+        scanf("%ld",&input);
+        mon=period_conv(input,&year,&date);
     }
     month=mon_data[mon-1];
-    printf("Period Entered: %d,%s,%d\n",date,month,year);
+    printf("Period Entered: %d,%s,%d\input",date,month,year);
     getchar();
     getchar();
     return 0;
 }
 
-int period_conv(long n, int *y, int *d)
+int period_conv(long input, int *y, int *d)
 {
     int temp, mon;
 
-    *y=n%10000;             /*1945*/
-    temp=n/10000;           /*1508*/
+    *y=input%10000;             /*1945*/
+    temp=input/10000;           /*1508*/
     mon=temp%100;           /*08*/
     *d=temp/100;            /*15*/
 
